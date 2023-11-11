a19fa99e7244ffc2a1dabb1b97f793e620e9c9ad
Michel Antonio <mr@michelantonio.com>
2023-11-11 16:18:40 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from dotenv import load_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        load_dotenv()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
